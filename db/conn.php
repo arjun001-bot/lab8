0716b426af70c29b18d4808e93cfa245f5ef8c81
Arjun <arjun001@algomau.ca>
2026-03-24 14:42:26 -0400

    Final fix - database connection updated

--- db/conn.php
+++ db/conn.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root"; // MAMP default
+$password = "";
 $dbname = "client";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
